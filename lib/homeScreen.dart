f4cc5efb474f6fde587aa8352c5ff8311f3dbd98
omedia23 <syedhasan.cse@gmail.com>
2022-06-16 14:10:22 +0600

    modified the ui

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -8,67 +8,137 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String result = '1728';
+  String resultTxt = '0.0';
+  final Color btnColor = Colors.grey.shade800;
+  final Color symbolColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              calButton('AC', Colors.grey, Colors.black),
-              calButton('+/-', Colors.grey, Colors.black),
-              calButton('%', Colors.grey, Colors.black),
-              calButton('/', Colors.amber.shade700, Colors.white),
-            ],
-          )
-        ]),
-      ),
-    );
-  }
-
-  Widget calButton(String btnsymbol, Color symbolColor, Color btnColor) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: OutlinedButton(
-        onPressed: () {},
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(btnColor),
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            resultWidget(),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        ),
-        child: Text(
-          btnsymbol,
-          style: TextStyle(
-            fontSize: getadaptiveTextSize(context, 20),
-            fontWeight: FontWeight.bold,
-            color: symbolColor,
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calButton('AC', Colors.grey, Colors.black),
+                calButton('+/-', Colors.grey, Colors.black),
+                calButton('%', Colors.grey, Colors.black),
+                calButton('/', Colors.amber.shade700, Colors.white),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calButton('7', btnColor, symbolColor),
+                calButton('8', btnColor, symbolColor),
+                calButton('9', btnColor, symbolColor),
+                calButton('X', Colors.amber.shade700, symbolColor),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calButton('4', btnColor, Colors.white),
+                calButton('5', btnColor, Colors.white),
+                calButton('6', Colors.grey.shade800, Colors.white),
+                calButton('-', Colors.amber.shade700, Colors.white),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calButton('1', Colors.grey.shade800, Colors.white),
+                calButton('2', Colors.grey.shade800, Colors.white),
+                calButton('3', Colors.grey.shade800, Colors.white),
+                calButton('+', Colors.amber.shade700, Colors.white),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calButton('0', btnColor, symbolColor),
+                calButton('.', btnColor, symbolColor),
+                Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: calButton('=', Colors.amber.shade700, symbolColor))
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 
-  Container resultText() {
-    return Container(
-      alignment: Alignment.centerRight,
+  SingleChildScrollView resultWidget() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              resultTxt,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 80,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  calculation(String symbol) {}
+
+  Widget calButton(String btnsymbol, Color btnColor, Color symbolColor) {
+    return OutlinedButton(
+      onPressed: () {
+        calculation(btnsymbol);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(btnColor),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+        ),
+      ),
       child: Text(
-        result,
-        style: const TextStyle(
-          fontSize: 28,
+        btnsymbol,
+        style: TextStyle(
+          fontSize: getadaptiveTextSize(context, 20),
           fontWeight: FontWeight.bold,
+          color: symbolColor,
         ),
       ),
     );
   }
-}
 
-dynamic getadaptiveTextSize(BuildContext context, dynamic value) {
-  // 720 is medium screen height
-  return (value / 720) * MediaQuery.of(context).size.height;
+  dynamic getadaptiveTextSize(BuildContext context, dynamic value) {
+    // 720 is medium screen height
+    return (value / 720) * MediaQuery.of(context).size.height;
+  }
 }
